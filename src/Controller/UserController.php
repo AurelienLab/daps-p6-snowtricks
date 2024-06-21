2b725c03411ff9d4036094f0f0e81f4f8c70fb41
Aurélien Lorence <aurelien@keyframe.fr>
2024-06-21 14:26:45 +0200

    feat(UserRole): User admin form Close #16

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
     public function __construct(
-        private readonly UserRepository $userRepository
+        private readonly UserRepository $userRepository, private readonly EntityManagerInterface $entityManager
     )
     {
     }
@@ -22,6 +26,26 @@ class UserController extends AbstractController
 
         return $this->render('user/index.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    #[Route('/user/{id}', name: 'app_user_edit')]
+    public function edit(User $user, Request $request): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRoles([$form->get('roles')->getData()]);
+
+            $this->entityManager->flush();
+            $this->addFlash('success', 'snowtricks.flashes.user_updated');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
         ]);
     }
 }
